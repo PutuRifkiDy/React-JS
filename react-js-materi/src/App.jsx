269de8afa9404992f0a63a74829310219c36408a
PutuRifkiDy <puturifki56@gmail.com>
2025-02-19 23:10:28 +0800

    Konsep Componen dan Props

--- react-js-materi/src/App.jsx
+++ react-js-materi/src/App.jsx
@@ -1,11 +1,43 @@
-function App() {
+// function component
+function Button() {
+  return (
+    <button className="bg-red-500 text-white px-5 py-2 rounded-xl shadow hover:bg-red-400">
+      Submit
+    </button>
+  );
+}
+
+// arrow function consep props
+const ButtonPropsKonsep = (props) => {
+  return (
+    <button className={`${props.backgroundColor} text-white px-5 py-2 rounded-xl shadow`}>
+      Submit
+    </button>
+  );
+}
+
+// konsep distractering dengan memberi default value pada props
+const ButtonDistractering = (props) => {
+  const { backgroundColor = "bg-green-500 hover:bg-green-300", children } = props;
+
+  return (
+    <button className={`${backgroundColor} text-white px-5 py-2 rounded-xl shadow`}>
+      {children}
+    </button>
+  );
+}
+
+
+export default function App() {
   return (
     <>
-      <div className="text-[25px]">
-        <p>hello world</p>
+      <div className="bg-blue-800 w-screen h-screen flex justify-center items-center gap-2">
+        <Button />
+        <ButtonPropsKonsep backgroundColor="bg-slate-600 hover:bg-slate-400" />
+        <ButtonDistractering backgroundColor="bg-yellow-500">Submit</ButtonDistractering>
       </div>
     </>
   )
 }
 
-export default App
+
