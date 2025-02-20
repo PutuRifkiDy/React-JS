d871c0c074038e29fbaba3b0e7c34f17c85da270
PutuRifkiDy <puturifki56@gmail.com>
2025-02-20 22:11:34 +0800

    React Router

--- react-js-materi/src/components/Layouts/AuthLayouts.jsx
+++ react-js-materi/src/components/Layouts/AuthLayouts.jsx
@@ -1,13 +1,15 @@
 const AuthLayouts = (props) => {
     const { children, title } = props;
     return (
-        <div className="w-full max-w-xs">
-            <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
-            <p className="font-medium text-slate-500 mb-8">
-                Welcome, Please enter your details
-            </p>
+        <div className="font-['Poppins'] flex justify-center items-center min-h-screen">
+            <div className="w-full max-w-xs">
+                <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
+                <p className="font-medium text-slate-500 mb-8">
+                    Welcome, Please enter your details
+                </p>
 
-            {children}
+                {children}
+            </div>
         </div>
     );
 }
