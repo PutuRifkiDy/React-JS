7c744a8c29ad8edfc9341e9d5f752341b7cbfb1e
PutuRifkiDy <puturifki56@gmail.com>
2025-02-21 13:45:26 +0800

    Conditional Rendering

--- react-js-materi/src/components/Layouts/AuthLayouts.jsx
+++ react-js-materi/src/components/Layouts/AuthLayouts.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 const AuthLayouts = (props) => {
     const { children, title } = props;
     return (
-        <div className="font-['Poppins'] flex justify-center items-center min-h-screen">
+        <div className="font-['Poppins'] flex flex-col justify-center items-center min-h-screen">
             <div className="w-full max-w-xs">
                 <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
                 <p className="font-medium text-slate-500 mb-8">
@@ -10,6 +11,22 @@ const AuthLayouts = (props) => {
 
                 {children}
             </div>
+            {title === "Register" && (
+                <p className="text-sm mt-5 text-center">
+                    Have an account?{" "}
+                    <Link to="/login" className="font-bold text-blue-600">
+                        Login
+                    </Link>
+                </p>
+            )}
+            {title === "Login" && (
+                <p className="text-sm mt-5 text-center">
+                    Don't have an account?{" "}
+                    <Link to="/register" className="font-bold text-blue-600">
+                        Sign up
+                    </Link>
+                </p>
+            )}
         </div>
     );
 }
